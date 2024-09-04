eef1ff632893c9025d2870c0859904afdb23f22a
Aden John <Adenthedev@gmail.com>
2024-09-04 15:32:22 -0600

    Initial commit

---
+++ app/src/main/java/com/example/dicerolling/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.dicerolling
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dicerolling.R.id.dice_image
+import com.example.dicerolling.ui.theme.DiceRollingTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+
+    lateinit var diceImage: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+          //  Toast.makeText(this,"Button Clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+     diceImage = findViewById(R.id.dice_image)
+
+    }
+
+    private fun rollDice() {
+
+        val randomInt= (1..6).random()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+        }
+}
